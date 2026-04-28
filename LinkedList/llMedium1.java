7ae18526f1b7180cbd5ca948dbe19e9aff1bf0c2
Matheus Guerra <mtsguerra9@gmail.com>
2026-04-28 10:53:59 +0100

    LLM1 - docs: refining Javadoc

--- LinkedList/llMedium1.java
+++ LinkedList/llMedium1.java
@@ -2,14 +2,23 @@ import java.util.*;
 
 class llMedium1 {
     /**
-     * <h1>LeetCode #143: Reorder List</h1>
+     * <strong>LeetCode #143: Reorder List</strong>
      *
      * <p>
      *     Approach: First start by finding the middle of the list (using
      *     Slow&Fast pointers). Then, invert the second half and merge it
      *     with the first half.
      * </p>
-     * @param head Head of the linked list
+     *
+     * <pre>
+     *     Complexity:
+     *     -Time: O(n), finding the middle takes O(n/2), reversing the second half takes O(n/2), merging it takes O(n/2), resulting in approx O(n)
+     *     -Space: O(1)
+     * </pre>
+     *
+     * @param head the head of the linked list
+     * @see
+     * <a href="https://leetcode.com/problems/reorder-list/">LeetCode 143</a>
      */
     public void reorderList(ListNode head) {
         if  (head == null || head.next == null) return;
@@ -19,7 +28,7 @@ class llMedium1 {
             firstHalf = firstHalf.next;
             secondHalf = secondHalf.next.next;
         }
-        secondHalf = reverseList(secondHalf.next);
+        secondHalf = reverseList(firstHalf.next);
         firstHalf.next = null;
         firstHalf = head;
         while (firstHalf != null && secondHalf != null) {
