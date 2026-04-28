47c2197cecef326cbf90f47a9bf30f8d8db88264
Matheus Guerra <mtsguerra9@gmail.com>
2026-04-28 10:22:43 +0100

    LLE3 - docs: refining Javadoc

--- LinkedList/llEasy3.java
+++ LinkedList/llEasy3.java
@@ -2,11 +2,20 @@ import java.util.*;
 
 class llEasy3 {
     /**
-     * My first solution, using a tortoise and hare logic to try to find a
-     * cycle. Where the hare jumps 2 node at each time, so if there is a
-     * cycle the hare will reach the tortoise at some point or reach the end.
-     * @param head head of the linked list
-     * @return boolean cycle
+     * <h1>LeetCode ex141. Linked List Cycle</h1>
+     *
+     * <h2>Approach: Using the tortoise and hare logic to find the cycle.</h2>
+     * <p>The tortoise and hare logic revolves around having two
+     * pointers, one that moves 1 node at a time (tortoise), and another
+     * moving 2 (hare).If there is a cycle they are bounded to find each
+     * other at some point.</p>
+     *
+     * <hr>
+     *     <b>Complexity: </b> This algorithm has O(n) time complexity and O
+     *     (1) space complexity
+     *
+     * @param head Head of the linked list
+     * @return Boolean cycle
      */
     public boolean hasCycle(ListNode head) {
         ListNode tortoise = head;
